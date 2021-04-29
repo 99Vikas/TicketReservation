9f359296151433b5c468ac167e35da4cd48dfe2a
Vikas <vschauhan0110@gmail.com>
2021-04-30 02:19:36 +0530

    Added test cases

--- src/test/java/com/railway/ticketReservation/TicketReservationApplicationTests.java
+++ src/test/java/com/railway/ticketReservation/TicketReservationApplicationTests.java
@@ -25,7 +25,7 @@ public class TicketReservationApplicationTests {
     @DisplayName("Test for finding ticket by id")
     public void getByIdTest() {
 
-        String id = "60845ded521a70456c81aae4";
+        String id = "6089cf05e476fa377bcab23d";
 
         Optional<Ticket> testTicket = ticketsRepo.findByTicketId(id);
 
@@ -35,7 +35,7 @@ public class TicketReservationApplicationTests {
     @Test
     @DisplayName("Test for getting PNR in Ticket Booking")
     public void getPNRTest() {
-        String pnr = "123520210415FAC-29";
+        String pnr = "20210414SL1";
         Optional<Ticket> testPnr = ticketsRepo.getTicketByPNR(pnr);
 
         assertNotNull(testPnr);
@@ -44,7 +44,7 @@ public class TicketReservationApplicationTests {
     @Test
     @DisplayName("Test for finding by pnr")
     public void findByPNR() {
-        String pnr = "123520210415FAC-29";
+        String pnr = "20210414SL1";
         Optional<Ticket> testPnr = ticketsRepo.findTicketByPNR(pnr);
 
         assertNotNull(testPnr);
